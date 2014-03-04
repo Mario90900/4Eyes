3031949204418936219aeb3701d60c1f2901ce63
Mario90900 <jnsecora@twcny.rr.com>
2014-03-04 12:20:10 -0500

    Started on Health for the Player
    Finished the spike room and started on the health scripts.

---
+++ 4Eyes Project/Assets/HealthControl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class HealthControl : MonoBehaviour
+{
+	public float health = 6f;
+	public float invincibleTime = 15f;
+
+	private float invincibleCount = invincibleTime;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+
