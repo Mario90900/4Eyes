b2588df5ee3ec6ad9a8505cdae36d97b05a9c253
Mario90900 <jnsecora@twcny.rr.com>
2014-03-05 12:40:08 -0500

    Continued on health
    Continued a bit of health, added in the Zelda Health UI example Terry
    found.

---
+++ 4Eyes Project/Assets/Scripts/HealthControl.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class HealthControl : MonoBehaviour
+{
+	public float health = 6f;
+	public float maxHealth = 6f;
+	public float invincibleTime = 15f;
+
+	private float invincibleCount;
+
+		// Use this for initialization
+		void Awake ()
+		{
+			invincibleCount = invincibleTime;
+		}
+
+		void heal (int amount)
+		{
+			if ((health + amount) >= maxHealth) 
+				health = maxHealth;
+			else
+				health += amount;
+		}
+
+		void hurt (int amount)
+		{
+			if ((Health - amount) <= 0f){
+				health = 0;
+				playerDie ();
+			} else {
+				health -= amount;
+			}
+}
+
